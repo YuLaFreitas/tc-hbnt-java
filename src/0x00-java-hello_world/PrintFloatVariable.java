de01b5ce94a99135e5f777c97576bac20e3c6118
Yuri F S Freitas <iruytj@gmail.com>
2022-01-27 09:15:33 -0300

    LIMPANDO SRC

--- src/0x00-java-hello_world/PrintFloatVariable.java
+++
@@ -1,6 +0,0 @@
-public class PrintFloatVariable {
-    public static void main(String[] args){
-        float pi = (float) Math.PI;
-        System.out.printf("Valor de pi: %.03f\n", pi);
-    }
-}
--- src/0x00-java-hello_world/PrintIntegerVariable.java
+++
@@ -1,9 +0,0 @@
-public class PrintIntegerVariable {
-    public static void main(String[] args){
-
-        int numero = 311;
-
-        System.out.println("Avenida Andaló, " + numero );
-
-    }
-}
