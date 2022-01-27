ae8b963d274c450a3f28c5419fc10ac2fb5344d1
Yuri Freitas <iruytj@gmail.com>
2022-01-27 08:57:13 -0300

    Initial commit

---
+++ src/0x01/numeros/Numeros0a99.java
@@ -0,0 +1,13 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+
+        for(int n = 0; n < 100; n++){
+            if(n==99){
+                System.out.print(n + "\n");
+            }else{
+                System.out.print(n + ", ");
+            }
+        }
+
+    }
+}
---
+++ src/com/own/Main.java
@@ -0,0 +1,15 @@
+package com.own;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int n = 0; n < 100; n++){
+            if(n==99){
+                System.out.print(n + "\n");
+            }else{
+                System.out.print(n + ", ");
+            }
+        }
+    }
+}
