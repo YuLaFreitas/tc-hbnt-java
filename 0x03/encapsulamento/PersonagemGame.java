783abeee65ca12ebe96f42f87af6a291293f4bb6
Yuri Freitas <iruytj@gmail.com>
2022-01-28 11:15:21 -0300

    TASK

---
+++ 0x03/encapsulamento/PersonagemGame.java
@@ -0,0 +1,20 @@
+public class PersonagemGame  {
+    private String nome;
+    private int SaudeAtual;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSaudeAtual() {
+        return SaudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.SaudeAtual = saudeAtual;
+    }
+}
