01fbf1b07089fcfc9046b3b208306dd185d08072
Yuri Freitas <iruytj@gmail.com>
2022-01-31 15:51:07 -0300

    TASK

---
+++ 0x04/quadrado/Retangulo.java
@@ -0,0 +1,32 @@
+public class Retangulo extends FormaGeometrica {
+    protected double largura;
+    protected double altura;
+
+    @Override
+    public double area(){
+        return largura * altura;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Retangulo] %.2f / %.2f",  largura, altura);
+    }
+
+    public void setLargura(int i) {
+        this.largura = test(i, "Largura");;
+
+    }
+
+    public void setAltura(int i) {
+        this.altura = test(i, "Altura");
+    }
+
+    public double test(int i, String word){
+        if(i > 0 ){
+            return i;
+        }else{
+            throw new IllegalArgumentException(word + " deve ser maior ou igual a 0");
+        }
+    }
+
+}
