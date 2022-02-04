52b911399179f0462924f9809360a9faa33573ae
Yuri Freitas <iruytj@gmail.com>
2022-02-04 15:13:59 -0300

    TASK

---
+++ 0x08/resumo_pedido/produtos/Livro.java
@@ -0,0 +1,48 @@
+package produtos;
+
+public class Livro extends Produto{
+    private int paginas;
+    private String autor;
+    private int edicao;
+
+
+    public Livro(String titulo, int ano, String pais,  double precoBruto, int paginas, String autor, int edicao) {
+        super(titulo, ano, pais, precoBruto);
+        setPaginas(paginas);
+        setAutor(autor);
+        setEdicao(edicao);
+    }
+
+    @Override
+    public double obterPrecoLiquido(){
+        return getPrecoBruto() + (getPrecoBruto()*0.15);
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+
+
+}
