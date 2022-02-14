c363b9c0aeac69fb131a0d296a520940d4c72922
Yuri Freitas <iruytj@gmail.com>
2022-02-14 11:16:20 -0300

    TASK

---
+++ 0x13/workflow/atividades/ModificarStatusVideo.java
@@ -0,0 +1,8 @@
+package atividades;
+
+public class ModificarStatusVideo implements Atividade {
+    @Override
+    public void execultar() {
+        System.out.println("modigicando status do video");
+    }
+}
