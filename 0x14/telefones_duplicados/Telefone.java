b08a32ac40c06469d7040a4b92b297693171dc40
Yuri Freitas <iruytj@gmail.com>
2022-02-16 17:15:53 -0300

    TASK

--- 0x14/telefones_duplicados/Telefone.java
+++ 0x14/telefones_duplicados/Telefone.java
@@ -26,10 +26,19 @@ public class Telefone{
     public int hashCode() {
         final int prime = 31;
         int result = 7;
+        result = prime * result + ((codigoArea == null) ? 0 : numero.hashCode());
+        result = prime * result + ((codigoArea == null) ? 0 : numero.hashCode());
+        return result;
+    }
+
+    /*@Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 7;
         result = prime * result + ((getCodigoArea() == null) ? 0 : getNumero().hashCode());
         result = prime * result + ((getCodigoArea() == null) ? 0 : getNumero().hashCode());
         return result;
-    }
+    }*/
 
     public String getCodigoArea() {
         return codigoArea;
