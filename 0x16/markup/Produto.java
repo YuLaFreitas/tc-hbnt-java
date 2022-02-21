11162221d4c94024594dacb216cc2ff465b4867b
Yuri Freitas <iruytj@gmail.com>
2022-02-21 11:13:46 -0300

    TASK

--- 0x16/markup/Produto.java
+++ 0x16/markup/Produto.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Produto {
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private double percentualMarkUp = 0.1;
+
+    public Produto(double preco, String nome) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    Consumer<Double> atualizarMarkUp = valor -> this.percentualMarkUp = (valor/100);
+    Supplier<Double> precoComMarkUp = () -> preco*(1+percentualMarkUp);
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
 }
+
