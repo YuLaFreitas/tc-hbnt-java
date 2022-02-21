b4071d0078aeb421dab6fcbee5aa0f32d8fe8272
Yuri Freitas <iruytj@gmail.com>
2022-02-21 11:43:39 -0300

    TASK

--- 0x16/liquido/FolhaPagamento.java
+++ 0x16/liquido/FolhaPagamento.java
@@ -1,6 +1,5 @@
 public class FolhaPagamento {
-    public static double calcularLiquido(Funcionario funcionario,
-                                         CalculadorDeSalario calculadorDeSalario){
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculadorDeSalario){
             return calculadorDeSalario.calcular(funcionario.getSalario());
     }
 }
