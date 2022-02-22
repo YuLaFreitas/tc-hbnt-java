3b527aea8bbcec46c8c5d103eded418c6d0032a8
Yuri Freitas <iruytj@gmail.com>
2022-02-22 15:35:20 -0300

    TASK

--- 0x18/maior_preco/Consulta.java
+++ 0x18/maior_preco/Consulta.java
@@ -4,12 +4,16 @@ import java.util.stream.Collectors;
 
 public class Consulta {
 
+    public static List<Produto> obterProdutosPorPrecoMinimo  (List<Produto> produtos, double precoMinimo){
+                return produtos.stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                        .collect(Collectors.toList());
+    }
+
     public static Produto obterProdutoMaiorPreco (List<Produto> produtos){
                 return produtos.stream()
                 .sorted(Comparator.comparing(Produto::getPreco).reversed()).collect(Collectors.toList())
                         .get(0);
-
-
     }
 
     public static List<Produto> obterLivrosDoPedido(Pedido pedido){
