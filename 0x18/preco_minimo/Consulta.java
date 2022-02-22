55cc4211b2d96abf0bced1222801b81a1cbdd28c
Yuri Freitas <iruytj@gmail.com>
2022-02-22 15:58:58 -0300

    TASK

--- 0x18/preco_minimo/Consulta.java
+++ 0x18/preco_minimo/Consulta.java
@@ -4,13 +4,6 @@ import java.util.stream.Collectors;
 
 public class Consulta {
 
-    public static List<Produto> obterProdutoMaiorPreco  (List<Produto> produtos, double precoMinimo){
-                return produtos.stream()
-                        .filter(min -> min.getPreco() >= precoMinimo)
-                .sorted(Comparator.comparing(Produto::getPreco).reversed())
-                        .collect(Collectors.toList());
-    }
-
     public static List<Produto> obterProdutosPorPrecoMinimo  (List<Produto> produtos, double precoMinimo){
                 return produtos.stream()
                         .filter(min -> min.getPreco() >= precoMinimo)
---
+++ 0x18/preco_minimo/Pedido.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Pedido {
+    private final int  codigo;
+    private List<Produto> produtos;
+    private final Cliente clientes;
+
+    public Pedido(int codigo, List<Produto> produtos, Cliente clientes) {
+        this.codigo = codigo;
+        this.produtos = produtos;
+        this.clientes = clientes;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public Cliente getClientes() {
+        return clientes;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 7;
+        result = prime * result + ((codigo == 0.0) ? 0 : codigo);
+        result = prime * result + ((produtos == null) ? 0 : produtos.hashCode());
+        result = prime * result + ((clientes == null) ? 0 : clientes.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pedido pedido = (Pedido) o;
+        return codigo == pedido.codigo && Objects.equals(produtos, pedido.produtos) && Objects.equals(clientes, pedido.clientes);
+    }
+}
