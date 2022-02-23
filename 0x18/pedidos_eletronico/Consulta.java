cb1f768dd65a4f6acca194b951815defbd78c512
Yuri Freitas <iruytj@gmail.com>
2022-02-23 14:34:23 -0300

    TASK

---
+++ 0x18/pedidos_eletronico/Consulta.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Consulta {
+    public static List<Pedido> obterPedidosComEletronicos (List<Pedido> pedidos){
+        return pedidos.stream().filter(
+                produto -> produto.getProdutos().stream().anyMatch(
+                        a -> a.getCategoria() == CategoriaProduto.ELETRONICO)
+        )
+                .collect(Collectors.toList());
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo  (List<Produto> produtos, double precoMinimo){
+                return produtos.stream()
+                        .filter(min -> min.getPreco() >= precoMinimo)
+                        .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco (List<Produto> produtos){
+                return produtos.stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed()).collect(Collectors.toList())
+                        .get(0);
+    }
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido){
+        List<Produto> livro = pedido.getProdutos();
+
+        return livro.stream().filter(book -> book.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+
+    }
+
+
+
+}
---
+++ 0x18/pedidos_eletronico/Produto.java
@@ -0,0 +1,58 @@
+import java.util.Objects;
+
+public class Produto{
+    private int  codigo;
+    private String nome;
+    private CategoriaProduto categoria;
+    private double preco;
+
+    public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getCodigo() +
+                "] " + getNome() +
+                " " + getCategoria() +
+                String.format(" R$ %.2f", getPreco());
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return codigo == produto.codigo && Double.compare(produto.preco, preco) == 0 && Objects.equals(nome, produto.nome) && categoria == produto.categoria;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 7;
+        result = prime * result + ((codigo == 0.0) ? 0 : codigo);
+        result = (int) (prime * result + ((preco == 0.0) ? 0 : preco));
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
+        return result;
+    }
+
+}
