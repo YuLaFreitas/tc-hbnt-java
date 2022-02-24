bb8fda87405cf6c6500a2582de2ad218b4e11148
Yuri Freitas <iruytj@gmail.com>
2022-02-24 16:11:29 -0300

    TASK

---
+++ 0x19/idade_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+
+    public static Map<String, List<Integer>>obterIdadePorCargo(List<Pessoa> pessoaList){
+       return pessoaList.stream()
+               .collect(
+                       Collectors.groupingBy(
+                               Pessoa::getCargo,
+                               Collectors.mapping(Pessoa::getIdade, Collectors.toList())
+                       )
+               );
+    }
+
+    public static Map<String, List<Pessoa>>obterPessoasPorCargo(List<Pessoa> pessoaList){
+       return pessoaList.stream().collect(Collectors.groupingBy(Pessoa::getCargo));
+    }
+}
