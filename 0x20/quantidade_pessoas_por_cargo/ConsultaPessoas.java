43835408f21e86b206f2f68e37157c988b9994ea
Yuri Freitas <iruytj@gmail.com>
2022-02-25 10:58:16 -0300

    TASK

---
+++ 0x20/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoaList){
+
+        return pessoaList.stream().collect(
+                Collectors.groupingBy(
+                        Pessoa::getCargo, Collectors.counting()
+                )
+        );
+    }
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoaList){
+
+        return pessoaList.stream().collect(
+                Collectors.groupingBy(
+                        Pessoa::getCargo, () ->  new TreeMap<>(Comparator.reverseOrder()
+                        ),
+                        Collectors.toCollection(TreeSet::new)
+                )
+        );
+    }
+}
