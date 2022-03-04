098459d388a6c3d1e96cc0322c524025d3fc1b46
Yuri Freitas <iruytj@gmail.com>
2022-03-04 11:20:59 -0300

    TASK

---
+++ 0x24/criar_jar/com/holbertonschool/matematica/Aritmeticos.java
@@ -0,0 +1,17 @@
+public class Aritmeticos {
+
+    public int somar(int x, int y){
+        return x+y;
+    }
+
+    public int subtrair(int x, int y){
+        return x-y;
+    }
+    public int multiplicar(int x, int y){
+        return x*y;
+    }
+
+    public int dividir(int x, int y){
+        return y != 0? x/y : 0;
+    }
+}
